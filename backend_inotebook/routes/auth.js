a332cad1a311a037f3a12c598c6c2c0bac88aee4
dhavlesuraj69@gmail.com <dhavlesuraj69@gmail.com>
2023-04-05 13:19:42 +0530

    add middleware function for get user

--- backend_inotebook/routes/auth.js
+++ backend_inotebook/routes/auth.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const fetchuser=require("../middleware/fetchuser");
 
-const jwt_SECRET="surajisgoodboy";
 
-//create a user using:POST "/api/auth/createuser".Doesn't require Auth
+const jwt_SECRET = "surajisgoodboy";
 
 // router.post(
 //   "/",(req, res) => {
@@ -22,7 +22,9 @@ const jwt_SECRET="surajisgoodboy";
 //     res.send(req.body);
 //    });
 
-router.get("/data", (req, res) => {   //send data to database using get reqvest
+//Route:1-create a user using:POST "/api/auth/createuser".Doesn't require Auth
+router.get("/data", (req, res) => {
+  //send data to database using get reqvest
   // eslint-disable-next-line no-undef
   console.log(req.body);
   const user = User(req.body);
@@ -30,7 +32,8 @@ router.get("/data", (req, res) => {   //send data to database using get reqvest
   res.send(req.body);
 });
 
-router.post(                  //send data using post reqvesy
+router.post(
+  //send data using post reqvesy
   //it using express validator
   "/createuser",
   [
@@ -52,66 +55,18 @@ router.post(                  //send data using post reqvesy
         return res.status(400).json({ error: "sorry user already exists" });
       }
 
-      const salt=await bcrypt.genSalt(10);        //it is created secuare password
-      const secPass=await bcrypt.hash(req.body.password,salt);
+      const salt = await bcrypt.genSalt(10); //it is created secuare password
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
-      user = await User.create({     //create a new user
+      user = await User.create({
+        //create a new user
         //it is return promises
         name: req.body.name,
         email: req.body.email,
         //password: req.body.password,
-        password:secPass
+        password: secPass,
       });
 
-      const data={               //method of json web tokan return secret code 
-        user:{
-          id:user.id          //in mango id has index and retrive data very fast for using id
-        }
-      }
-
-      const authtokan=jwt.sign(data,jwt_SECRET);
-      console.log(authtokan);
-
-      //res.send(req.body);
-      //res.json(user);
-      res.json({authtokan});
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("some error occured");
-    }
-  }
-);
-
-
-router.post(
-  //send data using post reqvesy
-  //it using express validator
-  "/loginuser",
-  [
-    body("email", "enter valid email").isLength({ min: 5 }),
-    body("password", "enter valid password").exists()
-  ],
-  async (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const {email,password}= req.body;
-    //eslint-disable-next-line no-undef
-    try {
-      let user = await User.findOne({email}); //check whether the email is exists already
-  
-      if (!user) {
-        return res.status(400).json({ error: "sorry user already exists"});
-      }
-
-      let passwordcompare= await bcrypt.compare(password,user.password);
-      
-      if(!passwordcompare)
-      {
-        return res.status(400).json({ error: "please try to login with correct credential" });
-      }
       const data = {
         //method of json web tokan return secret code
         user: {
@@ -131,5 +86,72 @@ router.post(
     }
   }
 );
+
+//Route:2-create a user using:POST "/api/auth/loginuser".Doesn't require Auth
+router.post(
+  //send data using post reqvesy
+  //it using express validator
+  "/loginuser",
+  [
+    body("email", "enter valid email").isLength({ min: 5 }),
+    body("password", "enter valid password").exists(),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body; //destructuring of request body
+    //eslint-disable-next-line no-undef
+    try {
+      let user = await User.findOne({ email }); //check whether the email is exists already using destructuring
+
+      if (!user) {
+        return res.status(400).json({ error: "sorry user already exists" });
+      }
+
+      let passwordcompare = await bcrypt.compare(password, user.password); //check password match or not using destructuring
+
+      if (!passwordcompare) {
+        return res
+          .status(400)
+          .json({ error: "please try to login with correct credential" });
+      }
+      const data = {
+        //method of json web tokan return secret code
+        user: {
+          id: user.id, //in mango id has index and retrive data very fast for using id
+        },
+      };
+
+      const authtokan = jwt.sign(data, jwt_SECRET);
+      console.log(authtokan);
+
+      //res.send(req.body);
+      //res.json(user);
+      res.json({ authtokan }); //send response
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
+
+//Route:3-create a user using:POST "/api/auth/getuser".Doesn't require Auth
+
+router.post("/getuser",fetchuser, async (req, res) => {    //fetchuser is middlware function
+  try {
+   const userId=req.user.id;
+   const user=await User.findById(userId).select("-password");
+
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some error occured");
+  }
+});
+
+
 
 module.exports = router;
